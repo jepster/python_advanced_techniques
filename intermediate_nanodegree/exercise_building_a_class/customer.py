ab58827e82e8c85b0065dc2a6328ee52065c2a14
Peter Majmesku <majmesku@publicplan.de>
2021-06-26 14:11:12 +0200

    Building a class practice

--- intermediate_nanodegree/exercise_building_a_class/customer.py
+++ intermediate_nanodegree/exercise_building_a_class/customer.py
@@ -1,12 +1,25 @@
 class Customer:
 
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, surname, tier=('free', 0)):
         self.first_name = first_name
-        self.last_name = last_name
+        self.surname = surname
+        self._tier = tier[0]
+        self._cost = tier[1]
+
 
     @property
     def name(self):
-        return self.first_name + ' ' + self.last_name
+        return f"{self.first_name} {self.surname}"
+
+    def can_access(self, content):
+        return content['tier'] == 'free' or content['tier'] == self._tier
+
+    @classmethod
+    def premium(cls, first_name, last_name):
+        return cls(first_name, last_name, tier=('premium', 10))
+
+    def bill_for(self, months):
+        return months * self._cost
 
 
 if __name__ == '__main__':
@@ -14,12 +27,12 @@ if __name__ == '__main__':
 
     marco = Customer('Marco', 'Polo')  # Defaults to the free tier
     print(marco.name)  # Marco Polo
-    # print(marco.can_access({'tier': 'free', 'title': '1812 Overture'}))  # True
-    # print(marco.can_access({'tier': 'premium', 'title': 'William Tell Overture'}))  # False
-    #
-    # victoria = Customer.premium("Alexandrina",
-    #                             "Victoria")  # Build a customer around the ('premium', 10$/mo) streaming plan.
-    # print(victoria.can_access({'tier': 'free', 'title': '1812 Overture'}))  # True
-    # print(victoria.can_access({'tier': 'premium', 'title': 'William Tell Overture'}))  # True
-    # print(victoria.bill_for(5))  # => 50 (5 months at 10$/mo)
-    # print(victoria.name)  # Alexandrina Victoria
+    print(marco.can_access({'tier': 'free', 'title': '1812 Overture'}))  # True
+    print(marco.can_access({'tier': 'premium', 'title': 'William Tell Overture'}))  # False
+
+    victoria = Customer.premium("Alexandrina",
+                                "Victoria")  # Build a customer around the ('premium', 10$/mo) streaming plan.
+    print(victoria.can_access({'tier': 'free', 'title': '1812 Overture'}))  # True
+    print(victoria.can_access({'tier': 'premium', 'title': 'William Tell Overture'}))  # True
+    print(victoria.bill_for(5))  # => 50 (5 months at 10$/mo)
+    print(victoria.name)  # Alexandrina Victoria
