17445a7b06887329a338893f2c978e99491ea129
Peter Majmesku <majmesku@publicplan.de>
2021-06-29 09:09:53 +0200

    Loading and filtering JSON file

--- intermediate_nanodegree/exercise_json_loading/nobel.py
+++ intermediate_nanodegree/exercise_json_loading/nobel.py
@@ -4,12 +4,26 @@ import helper
 
 
 def load_nobel_prizes(filename='prize.json'):
-    return {}
-
+    with open(filename) as f:
+        return json.load(f)
 
 def main(year, category):
     data = load_nobel_prizes()
-    # Add more here!
+    prizes = data['prizes']
+
+    for prize in prizes:
+        if 'laureates' not in prize:
+            continue
+        if category and prize['category'].lower() != category.lower():
+            continue
+        if year and prize['year'] != year:
+            continue
+
+        print(f"{prize['year']} Nobel Prize in {prize['category'].title()}")
+        for laureate in prize['laureates']:
+            firstname = laureate['firstname']
+            surname = laureate.get('surname', '')
+            print(f"{firstname} {surname}: {laureate['motivation']}")
 
 
 if __name__ == '__main__':
