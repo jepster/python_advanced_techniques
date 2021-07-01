7e8ec9133b7c5239f2417eaa5197debccade8fda
Peter Majmesku <majmesku@publicplan.de>
2021-07-01 18:26:14 +0200

    Finished pre-exercises

--- intermediate_nanodegree/project_near_earth_objects/small_pre_exercises/parse_json.py
+++ intermediate_nanodegree/project_near_earth_objects/small_pre_exercises/parse_json.py
@@ -1,14 +1,14 @@
 import json
 
 
-with open('../data/cad.json') as infile:
+with open('data/cad.json') as infile:
     jsonContent = json.load(infile)
 
     for item in jsonContent['data']:
         found_name = False
         found_date_start = False
         for i in item:
-            if type(i) == str and '2015 CL' == i:
+            if type(i) == str and '2002 PB' == i:
                 found_name = True
             if type(i) == str and '2000-Jan-01' in i:
                 found_date_start = True
